b91b2552c62958cb2e9479410148ab91f2bd1111
Ayush Jaiswal <aj85163@gmail.com>
2026-09-20 02:32:22 +0530

    implementing GAuth

--- android/app/src/main/java/com/ping/MainActivity.kt
+++ android/app/src/main/java/com/ping/MainActivity.kt
@@ -1,23 +1,21 @@
 package com.ping
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "Ping"
+override fun onCreate(savedInstanceState: Bundle?) {
+    // Switch from SplashTheme to AppTheme before super.onCreate.
+    setTheme(R.style.AppTheme)
+    super.onCreate(savedInstanceState)
+  }
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
+override fun getMainComponentName(): String = "Ping"
+
+override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
